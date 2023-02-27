aa5ba02684d082801ee169b23f6bca6a9911a085
Silpashree <silpashree@bootlabstech.com>
2023-02-27 18:58:16 +0530

    multiple rules

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
-# resource "aws_cloudwatch_event_bus" "messenger" {
-#   name = var.event_bus_name
-# }
 resource "aws_cloudwatch_event_rule" "console" {
- name        = var.event_rule_name
- schedule_expression = var.schedule_expression
+ for_each   = { for rule in var.eventrule_details : rule.event_rule_name => rule }   
+ name        = each.value.event_rule_name
+ schedule_expression = each.value.schedule_expression
 }
--- variables.tf
+++ variables.tf
@@ -2,19 +2,14 @@ variable "region" {
   description = "region"
   type        = string
 }
-# variable "event_bus_name" {
-#   description = "event bus name"
-#   type        = string
-# }
-variable "event_rule_name" {
-  description = "event rule name"
-  type        = string
+
+variable "eventrule_details" {
+  description = "eventrule_details"
+  type        = list (any)
 }
-variable "schedule_expression" {
-  description = "schedule_expression"
-  type        = string
-}
+
 variable "role_arn" {
   description = " The ARN of the IAM role"
   type = string
 }
+
